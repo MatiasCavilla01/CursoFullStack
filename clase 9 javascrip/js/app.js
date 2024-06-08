04978fcb823f053c869b3592f8fb3e1ef31b9679
MatiasCavilla01 <Matias.cavilla20038@outlook.com>
2024-06-08 03:21:11 -0300

    clase 9 javascript comienzo

---
+++ clase 9 javascrip/js/app.js
@@ -0,0 +1,29 @@
+console.log('hola mundo')
+let edad = 5
+
+/*operaciones compuestas*/
+if(edad>18){
+    console.log('Mayor de edad')
+} 
+else{
+    console.log('Menor de Edad')
+}
+
+//operador ternario solo se ultiliza en operaciones compuestas(if y else siemples)
+
+edad>18?console.log('Mayor de edad'):console.log('Menor de edad')
+
+//switch solo se puede usar cuando es condiciones ==
+
+let palabra = 'dog'
+switch(palabra){
+    case 'dog':
+        console.log('la traduccion es perro')
+        break
+    case 'table':
+        console.log('la traduccion es table')
+        break
+    default:
+        console.log('no se econtro la traduccion')
+
+}
